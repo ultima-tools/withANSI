4a76b5a33968fd062de9135ef13d0fa8920ff61b
guesswhozzz <torridtime@gmail.com>
2024-04-09 12:58:42 +0300

    Add error case

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 import { withANSI } from "./index.ts";
 
-console.log(withANSI(' Hello with ANSI ', ['dim:dim']));
+console.log(withANSI(' Hello with ANSI ', ['dim:flksdfjsdlkj']));
 
 // console.log('\033]Pg4040ff\033\\');
